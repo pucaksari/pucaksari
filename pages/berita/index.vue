eaa84c6eaaa7766f565e85f89c2b3020a836c1a8
alvinmr <maulanaalvin1@gmail.com>
2021-03-22 00:47:30 +0800

    ngubah nama folder artikel ke berita

---
+++ pages/berita/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <hero-blank />
+    <div class="container grid grid-cols-1 px-4 lg:grid-cols-2">
+      <Article />
+      <Article />
+      <Article />
+    </div>
+  </div>
+</template>
+
+<script>
+import HeroBlank from "~/components/HeroBlank.vue";
+export default {
+  components: { HeroBlank },
+};
+</script>
+
+<style>
+</style>
